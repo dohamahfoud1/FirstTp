587d3206118b760f2669b23fb80a95c6f049974a
doha mahfoud <dohamahfoud2003@gmail.com>
2024-02-21 11:57:49 +0100

    ajouter notes

--- etudiant.java
+++ etudiant.java
@@ -3,20 +3,22 @@ public class Etudiant {
     private String prenom;
     private int age;
     private String matricule;
+     private float note;
 
-    public Etudiant(String nom, String prenom, int age, String matricule) {
+    public Etudiant(String nom, String prenom, int age, String matricule, int note) {
         this.nom = nom;
         this.prenom = prenom;
         this.age = age;
         this.matricule = matricule;
+        this.note = note;
     }
 
     public void afficher() {
-        System.out.println("Nom: " + nom + ", Prénom: " + prenom + ", Âge: " + age + ", Matricule: " + matricule);
+        System.out.println("Nom: " + nom + ", Prénom: " + prenom + ", Âge: " + age + ", Matricule: " + matricule + ", note: " + note);
     }
 
     public static void main(String[] args) {
-        Etudiant etudiant1 = new Etudiant("Dupont", "Jean", 20, "123456");
+        Etudiant etudiant1 = new Etudiant("Dupont", "Jean", 20, "123456", 16.5);
         etudiant1.afficher();
     }
 }
